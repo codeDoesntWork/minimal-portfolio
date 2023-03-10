13c6b43866f53ca65b8891398a67f04ac5d15525
mahendra-N3ON <mahendradewangan2002@gmail.com>
2023-03-10 18:12:36 +0530

    initial commit

---
+++ js/loading-animation.js
@@ -0,0 +1,21 @@
+var t1 = new TimelineMax();
+
+t1.from(".ringOne", 1, {
+    delay: 0.1,
+    opacity: 0,
+    y:40,
+    ease: Expo.easeInOut
+}).from(".ringTwo", 1, {
+    delay: 0.2,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, "-=1").to(".ringOne", 2, {
+    delay: 0.1,
+    left: 350,
+    ease: Expo.easeInOut
+}).to(".ringTwo", 1, {
+    delay: 0.2,
+    left: 350,
+    ease: Expo.easeInOut
+},"-=1");
